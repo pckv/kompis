4d4ea9614acc646bdd1b36a3e37b671faa1e41bd
Geir <guggen33@gmail.com>
2019-10-23 18:00:20 +0200

    Created user entity and repository

---
+++ src/main/java/me/pckv/kompis/data/User.java
@@ -0,0 +1,24 @@
+package me.pckv.kompis.data;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@Entity
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Table(name = "\"user\"")
+public class User {
+    @Id @GeneratedValue private long id;
+    @NonNull private String email;
+    @NonNull private String password;
+    @NonNull private String displayName;
+}
+
