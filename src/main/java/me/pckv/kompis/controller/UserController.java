5d703519045cdd54cdffe0964ef07476768b412b
Geir <guggen33@gmail.com>
2019-10-27 13:53:44 +0100

    working on user repository and service

--- src/main/java/me/pckv/kompis/controller/UserController.java
+++ src/main/java/me/pckv/kompis/controller/UserController.java
@@ -1,24 +1,53 @@
 package me.pckv.kompis.controller;
 
 import me.pckv.kompis.data.User;
-import me.pckv.kompis.service.UserService;
-import org.apache.coyote.Response;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import me.pckv.kompis.data.UserRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class UserController {
 
-    private UserService userService;
+    private UserRepository repository;
+    // private UserService userService;
 
-    @RequestMapping("/users")
-    public User getUser() {
-        return userService.getUser;
+    UserController(UserRepository repository) {
+        this.repository = repository;
     }
 
-    @PostMapping("/create")
-    public Response createUser() {
+    @GetMapping("/users")
+    List<User> all() {
+        return repository.findAll();
+    }
 
+    @PostMapping("/users")
+    User newUser(@RequestBody User newUser) {
+        return repository.save(newUser);
+    }
+
+    @GetMapping("/users/{id}")
+    User one(@PathVariable Long id) {
+        return repository.getOne(id);
+    }
+
+    @PutMapping("/users/{id}")
+    User replaceUser(@RequestBody User newUser, @PathVariable long id) {
+        return repository.findById(id)
+                .map(user -> {
+                    user.setDisplayName(newUser.getDisplayName());
+                    user.setEmail(newUser.getEmail());
+                    user.setPassword(newUser.getPassword());
+                    return repository.save(user);
+                })
+                .orElseGet(() -> {
+                    newUser.setId(id);
+                    return repository.save(newUser);
+                });
+    }
+
+    @DeleteMapping("/user/{id}")
+    void deleteUser(@PathVariable long id) {
+        repository.deleteById(id);
     }
 }
