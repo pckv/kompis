4ce88f5cd017cc04677612fd84916851f91703bc
Geir <guggen33@gmail.com>
2019-10-27 14:24:39 +0100

    Finished user controller and service

--- src/main/java/me/pckv/kompis/controller/UserController.java
+++ src/main/java/me/pckv/kompis/controller/UserController.java
@@ -1,7 +1,7 @@
 package me.pckv.kompis.controller;
 
 import me.pckv.kompis.data.User;
-import me.pckv.kompis.data.UserRepository;
+import me.pckv.kompis.service.UserService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -9,45 +9,34 @@ import java.util.List;
 @RestController
 public class UserController {
 
-    private UserRepository repository;
-    // private UserService userService;
+    private UserService userService;
 
-    UserController(UserRepository repository) {
-        this.repository = repository;
+    UserController(UserService userService) {
+        this.userService = userService;
     }
 
     @GetMapping("/users")
     List<User> all() {
-        return repository.findAll();
+        return userService.getAllUsers();
     }
 
     @PostMapping("/users")
     User newUser(@RequestBody User newUser) {
-        return repository.save(newUser);
+        return userService.createUser(newUser);
     }
 
     @GetMapping("/users/{id}")
     User one(@PathVariable Long id) {
-        return repository.getOne(id);
+        return userService.getUser(id);
     }
 
     @PutMapping("/users/{id}")
     User replaceUser(@RequestBody User newUser, @PathVariable long id) {
-        return repository.findById(id)
-                .map(user -> {
-                    user.setDisplayName(newUser.getDisplayName());
-                    user.setEmail(newUser.getEmail());
-                    user.setPassword(newUser.getPassword());
-                    return repository.save(user);
-                })
-                .orElseGet(() -> {
-                    newUser.setId(id);
-                    return repository.save(newUser);
-                });
+        return userService.replaceUser(newUser, id);
     }
 
     @DeleteMapping("/user/{id}")
     void deleteUser(@PathVariable long id) {
-        repository.deleteById(id);
+        userService.deleteById(id);
     }
 }
--- src/main/java/me/pckv/kompis/service/UserService.java
+++ src/main/java/me/pckv/kompis/service/UserService.java
@@ -1,9 +1,46 @@
 package me.pckv.kompis.service;
 
 import me.pckv.kompis.data.User;
+import me.pckv.kompis.data.UserRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserService {
-    public User getUser;
+    private UserRepository repository;
+
+    public UserService(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<User> getAllUsers() {
+        return repository.findAll();
+    }
+
+    public User createUser(User newUser) {
+        return repository.save(newUser);
+    }
+
+    public User getUser(Long id) {
+        return repository.getOne(id);
+    }
+
+    public User replaceUser(User newUser, long id) {
+        return repository.findById(id)
+                .map(user -> {
+                    user.setDisplayName(newUser.getDisplayName());
+                    user.setEmail(newUser.getEmail());
+                    user.setPassword(newUser.getPassword());
+                    return repository.save(user);
+                })
+                .orElseGet(() -> {
+                    newUser.setId(id);
+                    return repository.save(newUser);
+                });
+    }
+
+    public void deleteById(long id) {
+        repository.deleteById(id);
+    }
 }
