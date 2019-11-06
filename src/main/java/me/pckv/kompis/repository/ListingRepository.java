34955c251a8bc446fdef6024b53a2d3311ab6438
Per Christian Kvalvik <pc@kvalvik.no>
2019-11-06 12:48:55 +0100

    Move repositories to separate branch

--- src/main/java/me/pckv/kompis/repository/ListingRepository.java
+++ src/main/java/me/pckv/kompis/repository/ListingRepository.java
@@ -1,5 +1,6 @@
-package me.pckv.kompis.data;
+package me.pckv.kompis.repository;
 
+import me.pckv.kompis.data.Listing;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ListingRepository extends JpaRepository<Listing, Long> {
