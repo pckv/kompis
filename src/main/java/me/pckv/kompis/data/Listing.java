0eb1e5f0bbd8ff84f91e36898aa91a5eabc5a3fd
espen <espenk95@gmail.com>
2019-11-06 16:16:00 +0100

    Change annotations from NotNull to NonNull in listing. Add default value to active and driver in listing. Add @Repository in ListingRepository

--- src/main/java/me/pckv/kompis/data/Listing.java
+++ src/main/java/me/pckv/kompis/data/Listing.java
@@ -2,13 +2,13 @@ package me.pckv.kompis.data;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.OneToOne;
-import javax.validation.constraints.NotNull;
+import javax.persistence.ManyToOne;
 
 @Data
 @Entity
@@ -16,7 +16,10 @@ import javax.validation.constraints.NotNull;
 @RequiredArgsConstructor
 public class Listing {
     @Id @GeneratedValue private long id;
-    @NotNull private String title;
-    @NotNull private boolean driver;
-    @NotNull @OneToOne private Location location;
+    @NonNull private String title;
+    @NonNull private boolean active = true;
+    @NonNull private boolean driver = false;
+    // @NonNull @OneToOne private Location location;
+    @ManyToOne private User owner;
+    @ManyToOne private User assignee;
 }
--- src/main/java/me/pckv/kompis/repository/ListingRepository.java
+++ src/main/java/me/pckv/kompis/repository/ListingRepository.java
@@ -2,6 +2,8 @@ package me.pckv.kompis.repository;
 
 import me.pckv.kompis.data.Listing;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ListingRepository extends JpaRepository<Listing, Long> {
 }
