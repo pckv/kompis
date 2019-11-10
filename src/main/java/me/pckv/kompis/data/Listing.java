fb0dd0fa9738517b47c942d82acd107c4b91f531
Per Christian Kvalvik <pc@kvalvik.no>
2019-11-10 20:07:18 +0100

    Remove @RequiredArgsConstructor and change @NonNull to @NotEmpty
    also added @Email to email field

--- src/main/java/me/pckv/kompis/data/Listing.java
+++ src/main/java/me/pckv/kompis/data/Listing.java
@@ -1,24 +1,22 @@
 package me.pckv.kompis.data;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.NonNull;
-import lombok.RequiredArgsConstructor;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotEmpty;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Entity
 @NoArgsConstructor
-@RequiredArgsConstructor
 public class Listing {
+
     @Id @GeneratedValue private long id;
-    @NonNull private String title;
-    @NonNull private boolean active = true;
-    @NonNull private boolean driver = false;
+    @NotEmpty private String title;
+    private boolean active = true;
+    private boolean driver = false;
     // @NonNull @OneToOne private Location location;
     @ManyToOne private User owner;
     @ManyToOne private User assignee;
--- src/main/java/me/pckv/kompis/data/User.java
+++ src/main/java/me/pckv/kompis/data/User.java
@@ -1,24 +1,23 @@
 package me.pckv.kompis.data;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.NonNull;
-import lombok.RequiredArgsConstructor;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Entity
 @NoArgsConstructor
-@RequiredArgsConstructor
 @Table(name = "\"user\"")
 public class User {
+
     @Id @GeneratedValue private long id;
-    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY) @NonNull private String email;
-    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY) @NonNull private String password;
-    @NonNull private String displayName;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY) @NotEmpty @Email private String email;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY) @NotEmpty private String password;
+    @NotEmpty private String displayName;
 }
